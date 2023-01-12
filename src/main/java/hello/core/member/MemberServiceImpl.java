9759a21665e014065afac8a4a25066dc2e6b6ad4
cyndii0502 <90389093+cyndii20@users.noreply.github.com>
2023-01-12 12:25:39 +0900

    Spring Basic Lec proj :: core 스프링 핵심 원리 이해 #2 수강

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
     //회원저장소 필요
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; //추상화에만 의존
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
